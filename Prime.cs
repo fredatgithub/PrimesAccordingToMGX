426e8dfbe0dc4310bf44ef259a7d7d25c575b6cb
MGX <admin@mgx.dev>
2025-06-18 09:34:21 -0700

    Initial project creation.

---
+++ Prime.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimeNumberCalculator
+{
+    public static class Prime
+    {
+        /// <summary>
+        /// Checks if a number is prime
+        /// </summary>
+        /// <param name="number">The number to check</param>
+        /// <returns>True if the number is prime, false otherwise</returns>
+        public static bool IsPrime(int number)
+        {
+            // Handle edge cases
+            if (number <= 1)
+                return false;
+            
+            if (number <= 3)
+                return true;
+            
+            // Quick check for even numbers
+            if (number % 2 == 0)
+                return false;
+            
+            // Check odd divisors up to the square root
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
+            
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            
+            return true;
+        }
+
+        /// <summary>
+        /// Gets all prime numbers less than a specified limit using the Sieve of Eratosthenes algorithm
+        /// </summary>
+        /// <param name="limit">The upper limit</param>
+        /// <returns>A list containing all prime numbers less than the limit</returns>
+        public static List<int> GetPrimesLessThan(int limit)
+        {
+            var primes = new List<int>();
+            
+            // Handle small limits
+            if (limit <= 2)
+                return primes;
+                
+            // Add 2 as the first prime
+            primes.Add(2);
+            
+            // For very small limits, we're done
+            if (limit <= 3)
+                return primes;
+                
+            // Use Sieve of Eratosthenes for larger limits
+            if (limit >= 1000)
+            {
+                return SieveOfEratosthenes(limit);
+            }
+            
+            // For smaller limits, use trial division which is simpler
+            for (int i = 3; i < limit; i += 2)
+            {
+                if (IsPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+            
+            return primes;
+        }
+        
+        /// <summary>
+        /// Implements the Sieve of Eratosthenes algorithm for finding primes efficiently
+        /// </summary>
+        /// <param name="limit">The upper limit</param>
+        /// <returns>A list containing all prime numbers less than the limit</returns>
+        private static List<int> SieveOfEratosthenes(int limit)
+        {
+            // Initialize the sieve with all numbers potentially prime
+            bool[] isPrime = new bool[limit];
+            for (int i = 2; i < limit; i++)
+            {
+                isPrime[i] = true;
+            }
+            
+            // Implement the sieve
+            for (int i = 2; i * i < limit; i++)
+            {
+                if (isPrime[i])
+                {
+                    // Mark all multiples of i as non-prime
+                    for (int j = i * i; j < limit; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+            
+            // Collect the primes into a list
+            var primes = new List<int>();
+            for (int i = 2; i < limit; i++)
+            {
+                if (isPrime[i])
+                {
+                    primes.Add(i);
+                }
+            }
+            
+            return primes;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PrimeNumberCalculator")]
+[assembly: AssemblyDescription("A console application that calculates prime numbers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeNumberCalculator")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d5c30b0-3e9a-4b8d-b833-4c40af6a3bf6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
